d359b91e7064884cdefd23aedfb7b76dc7c07292
shinya-tnk <zsla.sharc@gmail.com>
2020-10-18 17:32:28 +0900

    active_hashデータ作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :postage_type
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :preparation_day
+
+  validates :name, :description, :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :item_condition_id, numericality: { other_than: 1 }
+  validates :postage_type_id, numericality: { other_than: 1 }
+  validates :preparation_day_id, numericality: { other_than: 1 }
 end
