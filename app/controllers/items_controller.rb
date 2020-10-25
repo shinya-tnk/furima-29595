09ac33d46325dca716c031cc04a142583d4d46db
shinya-tnk <zsla.sharc@gmail.com>
2020-10-25 14:25:20 +0900

    select_boxへactive_hashデータ反映

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,16 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.find(item_params)
+    @item.create
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :image, :item_condition_id, :postage_type_id, :prefecture_id, :preparation_day_id, :price_id, :category_id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :category_id, numericality: { other_than: 1}
+    validates :item_condition, numericality: { other_than: 1}
+    validates :postage_type, numericality: { other_than: 1}
+    validates :prefecture, numericality: { other_than: 0}
+    validates :preparation_day, numericality: { other_than: 1}
+
+  end
+
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :postage_type
+  belongs_to_active_hash :preparation_day
+  belongs_to_active_hash :prefecture
+
 end
