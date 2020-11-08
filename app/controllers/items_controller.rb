a134af11d950c2384a2632ce49582a4a5642a672
shinya-tnk <zsla.sharc@gmail.com>
2020-11-08 14:56:38 +0900

    未ログインユーザーの新規登録ページへのリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
   def index
+    @items = Item.includes(:user).order('created_at DESC')
   end
+
   def new
     @item = Item.new
   end
 
   def create
-    @item = Item.find(item_params)
-    @item.create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
+
   def item_params
-    params.require(:item).permit(:name, :description, :image, :item_condition_id, :postage_type_id, :prefecture_id, :preparation_day_id, :price_id, :category_id)
+    params.require(:item).permit(:name, :description, :image, :price, :category_id, :item_condition_id, :postage_type_id, :prefecture_id, :preparation_day_id).merge(user_id: current_user.id)
   end
 end
