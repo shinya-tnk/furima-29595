fe271e8e192740e35bd18adda603ba6bde0ed1fd
shinya-tnk <zsla.sharc@gmail.com>
2020-11-15 16:18:00 +0900

    itemモデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  
   with_options presence: true do
+    validates :image
     validates :name, unless: :was_attached?
     validates :description
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, format: { with: /^[0-9]+$/ } }
@@ -9,9 +13,11 @@ class Item < ApplicationRecord
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :preparation_day_id, numericality: { other_than: 1 }
   end
-
-  belongs_to :user
-  has_one_attached :image
+  
+  def was_attached?
+    self.image.attached?
+  end
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :item_condition
@@ -19,7 +25,4 @@ class Item < ApplicationRecord
   belongs_to_active_hash :preparation_day
   belongs_to_active_hash :prefecture
 
-  def was_attached?
-    image.attached?
-  end
 end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品登録機能' do
+    context '出品登録がうまくいくとき' do
+      it 'すべて存在する' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品登録がうまくいかないとき' do
+      it "画像がない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "商品名がない" do
+        @item.image = nil
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "商品の説明がない" do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it "カテゴリー情報がない" do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it "商品の状態の情報がない" do
+        @item.item_condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition can't be blank")
+      end
+      it "配送料についての情報がない" do
+        @item.postage_type_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage type can't be blank")
+      end
+      it "発送元の地域についての情報がない" do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it "発送までの日数の情報がない" do
+        @item.preparation_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Preparation day can't be blank")
+      end
+      it "価格が未入力" do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "価格の範囲が¥300~¥9,999,999の間でない" do
+        @item.price = "200"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "販売価格が半角数字でない" do
+        @item.price = "６６６"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+    end
+  end
+end
