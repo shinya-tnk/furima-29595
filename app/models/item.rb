3801fc9e06277746d067bac9982e747151ff03d8
shinya-tnk <zsla.sharc@gmail.com>
2020-11-15 20:07:44 +0900

    with_optionsの記述場所修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  with_options presence: true, numericality: { other_than: 1 } do
+  with_options presence: true do
     validates :image
     validates :name
     validates :description
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, format: { with: /^[0-9]+$/ } }
+  end
+  with_options presence: true, numericality: { other_than: 1} do
     validates :category_id
     validates :item_condition_id
     validates :postage_type_id
