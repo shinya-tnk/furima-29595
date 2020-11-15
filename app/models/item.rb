00e8486bb88fc7628c40c25bdf3a5ffbb0699a91
shinya-tnk <zsla.sharc@gmail.com>
2020-11-15 19:51:04 +0900

    同一オプションのまとめ

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  with_options presence: true do
+  with_options presence: true, numericality: { other_than: 1 } do
     validates :image
     validates :name
     validates :description
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, format: { with: /^[0-9]+$/ } }
-    validates :category_id, numericality: { other_than: 1 }
-    validates :item_condition_id, numericality: { other_than: 1 }
-    validates :postage_type_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 0 }
-    validates :preparation_day_id, numericality: { other_than: 1 }
+    validates :category_id
+    validates :item_condition_id
+    validates :postage_type_id
+    validates :prefecture_id
+    validates :preparation_day_id
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
