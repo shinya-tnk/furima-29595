76e154f0887ff20be79b3130b5beadd7e6a02694
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2020-12-05 15:52:19 +0900

    ordersテーブル作成

---
+++ db/migrate/20201122095250_create_orders.rb
@@ -0,0 +1,9 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.integer :item_id
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
