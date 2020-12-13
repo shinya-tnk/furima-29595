6b48c27d5aa247264d13fa0150680f30cf95c16e
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2020-12-13 17:46:54 +0900

    クレジット決済機能修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -22,10 +22,10 @@ const pay = () => {
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
 
-      document.getElementById("number").removeAttribute("name");
-      document.getElementById("exp_month").removeAttribute("name");
-      document.getElementById("exp_year").removeAttribute("name");
-      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     })
