05c7f72ababfd390a49b288bc9b5488c478ec332
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2020-12-13 17:09:15 +0900

    ordersコントローラー記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,36 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    if @item
+      if current_user.id == @item.user_id
+        redirect_to root_path
+      end
+    end
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      pay_item
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
