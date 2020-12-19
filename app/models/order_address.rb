0e5d58775aa6a1d10a42d34c6e5524cab6d30074
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2020-12-19 18:46:44 +0900

    不要な記述の削除

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,7 +12,6 @@ class OrderAddress
   end
 
   def save
-    item = Item.new
     order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
