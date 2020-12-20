2d87bc0dcf9d881e293475d76bbaacd41b92d966
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2020-12-20 12:46:51 +0900

    userモデルFaker修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,9 +3,10 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
+
     nickname { Faker::Name.initials(number: 3) }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 3, min_numeric: 3) }
     password_confirmation { password }
     family_name { person.last.kanji }
     first_name { person.first.kanji }
